d3177f83c0ae53a2d9649f813111464f867eef2f
yan souza <yan17souza@hotmail.com>
2022-04-18 22:30:46 -0300

    commiting some stuff

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let feedback = [
+		{
+			id: '1',
+			rating: '10',
+			text: 'Qui aute ex esse ea enim do nostrud reprehenderit cillum cupidatat consequat.',
+		},
+		{
+			id: '2',
+			rating: '6',
+			text: 'Qui aute ex esse ea enim do nostrud reprehenderit cillum cupidatat consequat.',
+		},
+		{
+			id: '3',
+			rating: '9',
+			text: 'Qui aute ex esse ea enim do nostrud reprehenderit cillum cupidatat consequat.',
+		}
+	]
+	
+</script>
+
+<main>
+	<h1>Hello</h1>
+</main>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,6 @@
+<script>
+
+</script>
+
+
+Feedback List
